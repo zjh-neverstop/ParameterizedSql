f792db40ded1526342dbeb3dd3b2850fa5fe02c3
zhaojinghao <zhaojinghao1986@126.com>
2015-03-11 13:22:41 +0800

    添加方法

--- BLL/CommonBll.cs
+++ BLL/CommonBll.cs
@@ -12,11 +12,16 @@ namespace BLL
     {
         public static string Information;
 
-        public static DataSet GetDataByParameterizedSql(string sql, List<CustomSqlParam> paramList)
+        /// <summary>
+        /// 根据sql语句获取结果
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns></returns>
+        public static DataSet GetDataBySql(string sql)
         {
             try
             {
-                return CommonDal.GetDataByParameterizedSql(sql, paramList);
+                return CommonDal.GetDataBySql(sql);
             }
             catch (ConnectionException Ex)
             {
@@ -33,6 +38,99 @@ namespace BLL
                 Errors.WriteLog(Ex.Message, Ex);
                 Information = Ex.Message;
                 return null;
+            }
+        }
+
+        /// <summary>
+        /// 根据参数化sql语句获取结果
+        /// </summary>
+        /// <param name="sql">参数化sql语句</param>
+        /// <param name="paramList">参数对象列表</param>
+        /// <returns></returns>
+        public static DataSet GetDataByParameterizedSql(string sql, List<CustomSqlParam> paramList)
+        {
+            try
+            {
+                return CommonDal.GetDataByParameterizedSql(sql,paramList);
+            }
+            catch (ConnectionException Ex)
+            {
+                Information = Ex.Message;
+                return null;
+            }
+            catch (DataAccessExcption Ex)
+            {
+                Information = Ex.Message;
+                return null;
+            }
+            catch (Exception Ex)
+            {
+                Errors.WriteLog(Ex.Message, Ex);
+                Information = Ex.Message;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 执行参数化sql语句
+        /// </summary>
+        /// <param name="sql">参数化sql语句</param>
+        /// <param name="paramList">参数对象列表</param>
+        /// <returns></returns>
+        public static bool ExecuteParameterizedSql(string sql, List<CustomSqlParam> paramList)
+        {
+            try
+            {
+                int affectcount = CommonDal.ExecuteParameterizedSql(sql, paramList);
+                Information = "操作成功,共影响[" + affectcount.ToString() + "]条记录!";
+                return true;
+            }
+            catch (ConnectionException Ex)
+            {
+                Information = Ex.Message;
+                return false;
+            }
+            catch (DataAccessExcption Ex)
+            {
+                Information = Ex.Message;
+                return false;
+            }
+            catch (Exception Ex)
+            {
+                Errors.WriteLog(Ex.Message, Ex);
+                Information = Ex.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 执行参数化事务语句
+        /// </summary>
+        /// <param name="sqlDirectionary"></param>
+        /// <returns></returns>
+        public static bool ExecuteTransSql(Dictionary<String, List<CustomSqlParam>> sqlDirectionary)
+        {
+            try
+            {
+                int affectcount = CommonDal.ExecuteTransSql(sqlDirectionary);
+                Information = "操作成功";
+                return true;
+            }
+            catch (ConnectionException Ex)
+            {
+                Information = Ex.Message;
+                return false;
+            }
+            catch (DataAccessExcption Ex)
+            {
+                Information = Ex.Message;
+                return false;
+            }
+            catch (Exception Ex)
+            {
+                Errors.WriteLog(Ex.Message, Ex);
+                Information = Ex.Message;
+                return false;
             }
         }
     }
